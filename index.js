384cc37173ddabd3b12e133db12d91176cde2e47
Abhay Pratap Singh <abhayalternativeid@gmail.com>
2025-09-01 23:20:17 +0530

    new changes

---
+++ index.js
@@ -0,0 +1,11 @@
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
+
+//YOUR CODE GOES DOWN HERE:
+let fullAddress=restaurant['address',"city","state","zipcode"];
+console.log(fullAddress);

bea3a2f032fb3ad04770a165140ea80af5de2085
Abhay Pratap Singh <abhayalternativeid@gmail.com>
2025-09-06 19:58:29 +0530

    ----

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const restaurant = {
-    name: 'Ichiran Ramen',
-    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
-    city: 'Brooklyn',
-    state: 'NY',
-    zipcode: '11206',
-}
+// Guess the Number Game
 
-//YOUR CODE GOES DOWN HERE:
-let fullAddress=restaurant['address',"city","state","zipcode"];
-console.log(fullAddress);
+// let max=parseInt(prompt("Enter the maximum number:"));
+// while(!max){
+//     max=parseInt(prompt("Enter a valid number:"));
+// }
+// const targetNum=Math.floor(Math.random()*max)+1;
+// console.log(targetNum);
+// let guess=parseInt(prompt("Enter your first guess:"));
+// let attempts=1;
+// while(parseInt(guess)!==targetNum){
+//     if(guess==='q')break;
+//     attempts++; 
+//     if(guess>targetNum){
+//         guess=prompt("Too high! Enter a new guess:");
+//     }else{
+//         guess=prompt("Too low! Enter a new guess:");
+//     }
+// }
+// if(guess==='q'){
+//     console.log("You quit the game.");
+// }else{
+//     console.log(`You got it! It took you ${attempts} guesses.`);
+// }
+
+
+// To Do List Application
+
+// let input=prompt("Start The To-Do list ? yes/quit");
+// while(input!=='quit'){
+// input=prompt("What would you like to do? (new/list/delete/quit)");
+// console.log(input);
+// }
+// console.log("You quit the To-Do List");
